67338707fceb2306fd7d7d9a0864c6afbfb55894
Solanya <paul.corlay@gmail.com>
2016-05-10 18:40:46 +0200

    Additional comments

--- app/src/main/java/com/vtm/solanya/imgtrt/otTERApplication.java
+++ app/src/main/java/com/vtm/solanya/imgtrt/otTERApplication.java
@@ -6,15 +6,17 @@ import android.content.Context;
 import org.acra.*;
 import org.acra.annotation.*;
 
+// Le seul intérêt de cette partie consiste à pouvoir récupérer les messages d'erreur lors de l'utilisation de l'application
+// par les utilisateurs.
+
 @ReportsCrashes(
-        formUri = "https://collector.tracepot.com/dd5666d9"
+        formUri = "https://collector.tracepot.com/dd5666d9"     // Changer cette adresse pour changer la destination des messages d'erreurs
 )
 public class otTERApplication extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
 
-        // The following line triggers the initialization of ACRA
         ACRA.init(this);
     }
 }
